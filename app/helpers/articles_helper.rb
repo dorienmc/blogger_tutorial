ecf33de545f9bc10d2a5d0099a2f3ea8e130d569
Dorien Meijer Cluwen <dorien.meijercluwen@nvc3141.nedap.local>
2017-07-14 16:01:39 +0200

    I3 Tagging

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,7 +2,7 @@ module ArticlesHelper
 
   # Method used to filter out which parameters from the front-end we want to accept/require
   def article_params
-    params.require(:article).permit(:title, :body)
+    params.require(:article).permit(:title, :body, :tag_list)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Blogger::Application.routes.draw do
   root to: 'articles#index'
+
   resources :articles do
     resources :comments #added in a loop because its a sub-resource
   end
+  resources :tags
 end
