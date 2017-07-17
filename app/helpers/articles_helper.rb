d10c8c45561f35aae623ea346d87159fe3b19f34
Dorien Meijer Cluwen <dorien.meijercluwen@nvc3141.nedap.local>
2017-07-17 17:03:34 +0200

    I4 Paperclip gem

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,7 +2,7 @@ module ArticlesHelper
 
   # Method used to filter out which parameters from the front-end we want to accept/require
   def article_params
-    params.require(:article).permit(:title, :body, :tag_list)
+    params.require(:article).permit(:title, :body, :tag_list, :image)
   end
 
 end
